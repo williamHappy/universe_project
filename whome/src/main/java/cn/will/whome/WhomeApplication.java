78621b5f4cf0f6ee3aee1d9952e74dfa6f4e6f8f
williamhappy <ctwang1994@163.com>
2018-01-21 01:56:57 +0800

    create repo 2018/01/21 universe_project

---
+++ whome/src/main/java/cn/will/whome/WhomeApplication.java
@@ -0,0 +1,12 @@
+package cn.will.whome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhomeApplication.class, args);
+	}
+}
---
+++ whome/src/main/java/cn/will/whome/controller/IndexController.java
@@ -0,0 +1,18 @@
+package cn.will.whome.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Desc:
+ * Author: will
+ * Date: 18:19 2018/1/19
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index_new";
+    }
+}
