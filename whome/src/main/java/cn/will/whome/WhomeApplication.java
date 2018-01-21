8acd42a0b311158d30cb920757c82a14cc8a71a7
williamHappy <ctwang1994@163.com>
2018-01-21 16:27:10 +0800

    update 2018/01/21 springboot run way

--- whome/src/main/java/cn/will/whome/WhomeApplication.java
+++ whome/src/main/java/cn/will/whome/WhomeApplication.java
@@ -2,9 +2,10 @@ package cn.will.whome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WhomeApplication {
+public class WhomeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WhomeApplication.class, args);
